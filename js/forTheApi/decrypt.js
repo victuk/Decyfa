849bb86d312bbd2c5186f1ae0b0b0ab5a343be86
Victor <ukokjnr@gmail.com>
2022-06-16 21:25:27 +0100

    Working properly

--- js/forTheApi/decrypt.js
+++ js/forTheApi/decrypt.js
@@ -39,23 +39,40 @@ seperateImageButton.addEventListener('click', async function(e) {
     }
 });
 
-decryptButton.addEventListener('click', async function(e) {
-    e.preventDefault();
-    let res = await fetch(baseUrl + "decrypt-text", {
-        mode: "no-cors",
-        method: 'POST',
+
+decryptButton.addEventListener('click', async function() {
+
+    // console.log(JSON.stringify(privateKey.value));
+
+    // let stringToSend = JSON.stringify({
+    //     ciphertext: ciphertext.value,
+    //     decryptionKey: privateKey.value
+    // });
+
+    // let stringToSend = JSON.stringify({
+    //     ciphertext: 'YXy4aLtRjlN2ltwgdGOr1eHZjiDT8b-Ww3bOJ6HU4rEUP5yoxEwLgll67dZhLUKAYSPTYqqmHxEHiD06Wml_F7hPmKhbmmkKjP48Ei8uo-fo0turfIcGw31P_o6ME_31ZmNQt4RiwVWXBKcpL8vSGm_mjOGU4zGqid9ET6BaBms=',
+    //     decryptionKey: '-----BEGIN RSA PRIVATE KEY-----\nMIICYAIBAAKBgQCK1gFilD3cDbb88AJ5pmzrdhVl3hNmZXGLNmwvzna8MeMinBN7\ns5vX8r68zF9a5+/aNrznBb1Az72elJ2Hz0j0BOSIKHeL56PRmwQPin9xa4DEzrRK\ne2122/ZpmjB5MHsIrStPtFY1Yp+EJYlPd4ubhEdslQTYJysOLQK+3OXPWQIDAQAB\nAoGAdTEgCi5H6TBKfke6TyWEW11ZwpsqlQycQ36048XLxB/4Moa8i3aY3jgxePyq\noPmXJQN/gSL/FW5GCU45E18bHVUSwXY39LB++b7OVS859ysfdLj/iE+3RkYRTCU4\nWKG4qJdz/1tQF6M2Au2qN3bSHyTcqG4viIC6NTuFEDna64ECRQCN4VuzyW/GRVuY\nnLiHQ1uuqBpgqsHEWfuw6gI/RQt1GoK8abyWm52Ieq7AAsGyAXbCSqRdxIQAYK5S\n8Helc39ca6mijQI9APqByMX/+kDi6c2LyX1KT4hIDSUSW9WVb43C4A9vz01UiZW/\nsoIXbVmlH4gIXppODmRC3vLV33xxvKtS/QJEMT8z8WfOIGgarxJXCrxaqnk3Dq62\nq00AjBvtw8BXhcI59uRMzYoeWC0dUxCh4wOXEUGPY+y+VNM6LxfAr/EzGAVW0FEC\nPQCkHhMjZSjXCdKTT1maM+0XI904xGZV76k9/sY8VsxBpXjeGtEgWajSalPWE2Wn\nQiw7k3PtNCFOrlUlCSECRAyPjmAuLLGFHIyhMDGJ88isi70juSxLx7a1LEynvbe9\ngApZ7T0YpFOQqh0V7ErrWjl4VH41MjIJxNeBdApIe0WgMHjm\n-----END RSA PRIVATE KEY-----\n'
+    // });
+
+
+    // console.log(stringToSend)
+
+    
+    fetch("http://localhost:8000/decrypt-text", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-            decryptionKey: privateKey.value,
-            token: localStorage.getItem('token')
-        },
-        body: JSON.stringify({
-            ciphertext: ciphertext.value
-        })
+            "Content-Type": "application/json",
+            token: localStorage.getItem('token'),
+            ciphertext: ciphertext.value,
+            decryptionKey: privateKey.value
+        }
+    }).then(res => res.json())
+    .then(response => {
+        console.log(response);
     });
 
-    const response = await res.json();
+    // const response = await resTwo.json();
 
-    console.log(response);
+    // console.log(response);
 });
 
--- js/forTheApi/encrypt.js
+++ js/forTheApi/encrypt.js
@@ -1,16 +1,16 @@
 // Encrypt a text
-const plainText = document.getElementById('plaintext');
-const publicKey = document.getElementById('publicKey');
-const cyphertext = document.getElementById('encrypted-text');
-const encryptTextButton = document.getElementById('encryptTextButton');
+const plainText = document.getElementById("plaintext");
+const publicKey = document.getElementById("publicKey");
+const cyphertext = document.getElementById("encrypted-text");
+const encryptTextButton = document.getElementById("encryptTextButton");
 
 // Hide encrypted text into image
-const f5keys = document.getElementsByClassName('f5keys');
-const email = document.getElementById('email');
-const cyphertextToHide = document.getElementById('ciphertext-to-hide');
-const image = document.getElementById('image');
-const hideAndSendButton = document.getElementById('hideImageAndSend');
-const showStatus = document.getElementById('showStatus');
+const f5keys = document.getElementsByClassName("f5keys");
+const email = document.getElementById("email");
+const cyphertextToHide = document.getElementById("ciphertext-to-hide");
+const image = document.getElementById("image");
+const hideAndSendButton = document.getElementById("hideImageAndSend");
+const showStatus = document.getElementById("showStatus");
 
 encryptTextButton.addEventListener('click', async function(e) {
     e.preventDefault();
@@ -20,12 +20,10 @@ encryptTextButton.addEventListener('click', async function(e) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            token: localStorage.getItem('token')
-        },
-        body: JSON.stringify({
+            token: localStorage.getItem('token'),
             text: plainText.value,
             encryptionKey: publicKey.value
-        })
+        }
     });
 
     const response = await res.json();
@@ -35,42 +33,40 @@ encryptTextButton.addEventListener('click', async function(e) {
     }
 });
 
+hideAndSendButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  hideAndSendButton.innerText = "Sending...";
+  let keysArr = [];
+  for (let i = 0; i < f5keys.length; i++) {
+    keysArr.push(f5keys[i].value);
+  }
 
-hideAndSendButton.addEventListener('click', async function(e) {
-    e.preventDefault();
-    hideAndSendButton.innerText = "Sending...";
-    let keysArr = [];
-    for(let i = 0; i < f5keys.length; i++) {
-        keysArr.push(f5keys[i].value);
-    }
-    
-    const neededKey = keysArr.join(',');
+  const neededKey = keysArr.join(",");
 
-    let formdata = new FormData();
+  let formdata = new FormData();
 
-      formdata.append('file', image.files[0]);
+  formdata.append("file", image.files[0]);
 
+  const res = await fetch(baseUrl + "hide-text-in-image", {
+    method: "POST",
+    headers: {
+      token: localStorage.getItem("token"),
+      f5key: neededKey,
+      textToHide: cyphertextToHide.value,
+      email: email.value,
+    },
+    body: formdata,
+  });
 
-    const res = await fetch(baseUrl + 'hide-text-in-image', {
-        method: 'POST',
-        headers: {
-            token: localStorage.getItem('token'),
-            f5key: neededKey,
-            textToHide: cyphertextToHide.value,
-            email: email.value
-        },
-        body: formdata
-    });
+  const response = await res.json();
 
-    const response = await res.json();
-
-    if(response.status) {
-        hideAndSendButton.innerText = "Hide cyphertext in image and send";
-        showStatus.innerText = "Successful";
-    } else {
-        hideAndSendButton.innerText = "Hide cyphertext in image and send";
-        showStatus.innerText = "An error has occured";
-    }
+  if (response.status) {
+    hideAndSendButton.innerText = "Hide cyphertext in image and send";
+    showStatus.innerText = "Successful";
+  } else {
+    hideAndSendButton.innerText = "Hide cyphertext in image and send";
+    showStatus.innerText = "An error has occured";
+  }
 });
 
 /* '-----BEGIN RSA PUBLIC KEY-----
