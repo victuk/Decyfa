cc4346a4ac193c32490a7cbec429c97c20cc414f
Victor <ukokjnr@gmail.com>
2022-06-28 11:15:07 +0100

    N

---
+++ js/forTheApi/handleKeyRequests.js
@@ -0,0 +1,12 @@
+const email = document.getElementById('email');
+const sendRequestButton = document.getElementById('sendRequest');
+const requests = document.getElementById('requests');
+
+// setInterval(() => {
+//     fetch(baseUrl + '', {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type'
+//         }
+//     });
+// }, 5000);
