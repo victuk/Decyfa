13d3fbfda6e667c7a78932febb0cbacde491aa65
Victor Peter Ukok <38268008+victuk@users.noreply.github.com>
2023-01-14 19:52:58 +0100

    Update baseUrl.js

--- js/baseUrl.js
+++ js/baseUrl.js
@@ -1,4 +1,4 @@
 // const baseUrl = 'http://127.0.0.1:8000/';
-const baseUrl = 'https://steg-api.herokuapp.com/';
+const baseUrl = 'https://steg-api.onrender.com/';
 
 // https://steg-api.herokuapp.com/
